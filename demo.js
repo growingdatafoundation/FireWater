f27b34ab0f2f6cd950f384ab01e92eaffb14d899
Anoop Mishra <mishra.anoop6@gmail.com>
2020-08-16 10:23:52 +0930

    Minor changes

--- demo.js
+++ demo.js
@@ -1,17 +1,21 @@
-var apikey = 'M3F1H0aDtDmkjvHj2NdMVK4HnEYi0V0Z16CvUiwuHzk';
-var zoomLevel = 14;
-var routeInstructionsContainer = document.getElementById('panel');
-var wfsUrl = 'https://geo.opensensing.at/cgi-bin/qgis_mapserv.fcgi?SERVICE=WFS&VERSION=2.0&REQUEST=getFeature&typeName=firewater&MAP=/home/phartl/geo/firehack.qgs&outputFormat=application/json'
+var apikey = 'HiQJYJvRF0bzY3C1FDReYh8VXa8a2LFRnUUthlwGUG4';
+var zoomLevel = 10;
+var panel = document.getElementById('panel');
+//var dataUrl = 'https://geo.opensensing.at/cgi-bin/qgis_mapserv.fcgi?SERVICE=WFS&VERSION=2.0&REQUEST=getFeature&typeName=firewater&MAP=/home/phartl/geo/firehack.qgs&outputFormat=application/json';
+var dataUrl = 'https://firewater.opensensing.com/api/firewaters';
+var routes = [];
 var platform = new H.service.Platform({
   apikey: apikey
 });
+var count = 0;
+var idx = 0;
 var defaultLayers = platform.createDefaultLayers();
 
 //Step 2: initialize a map - this map is centered over Europe
 var map = new H.Map(document.getElementById('map'),
   defaultLayers.vector.normal.map,{
   center: {lat:50, lng:5},
-  zoom: 4,
+  zoom: 9,
   pixelRatio: window.devicePixelRatio || 1
 });
 // add a resize listener to make sure that the map occupies the whole container
@@ -25,15 +29,54 @@ var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 // Create the default UI components
 var ui = H.ui.UI.createDefault(map, defaultLayers);
 
-//add a marker
-var adelaide = new H.map.Marker({lat:-34.92874438537443, lng:138.5987412929535});
-map.addObject(adelaide);
 
 map.setCenter({lat:-34.92874438537443, lng:138.5987412929535});
 map.setZoom(zoomLevel);
+var layers = [];
+var base = 'Hydrants.zip';
+
+
+getData(function(response){
+ for(var i in response) {
+    console.log("anpop")
+      //add a marker
+      var marker = new H.map.Marker({lat:response[i].lat, lng:response[i].lng});
+      map.addObject(marker);
+  }
+ 
+ /*
+  group = new H.map.Group();
+group.addObject(marker1);
+group.addObject(marker2);
+
+// add to map
+map.addObject(group);
+shp(base).then(function(geojsonObject){
+  console.log(geojsonObject[0].features)
+  var icon = new H.map.Icon('hydrant-icon.png')
+  for(var i in geojsonObject[0].features) {
+      //add a marker
+      var marker = new H.map.Marker({lat:geojsonObject[0].features[i].geometry.coordinates[1], lng:geojsonObject[0].features[i].geometry.coordinates[0]}, {icon: icon});
+      group.addObject(marker);
+  }
+
+}); */
+  $('body').addClass('loaded');
+});
 
 
 //------------- Function list --------------
+function getData(callback) {
+ //// $.ajax({
+  //  url: dataUrl,
+  //  type: "GET",
+  //  success : function (response) {
+      callback([{"id":2,"device_id":4,"lat":"-33.352474","lng":"138.18175","device_name":"pls2-l-0066","toas":null,"cap":null,"lev":"15826","con":null,"geom":"0101000020E6100000894160E5D04561405C5837DE1DAD40C0","time_updated":"2020-08-15 08:48:37.296055"},{"id":1,"device_id":3,"lat":"-35.025856","lng":"138.52568","device_name":"pls2-l-0067","toas":null,"cap":null,"lev":"11619","con":null,"geom":"0101000020E61000002905DD5ED2506140E333D93F4F8341C0","time_updated":"2020-08-15 10:45:58.587888"}]);
+     //callback('http://localhost:8080/data.json')
+  //  }
+//  });
+}
+
 function getCurrentLocation(){ 
   if (!navigator.geolocation){
     alert("<p>Sorry, your browser does not support Geolocation</p>");
@@ -43,21 +86,30 @@ function getCurrentLocation(){
 }
 
 function success(position) {
-  console.log("Does it come inside")
+ // $('body').removeClass('loaded');
   var currentPos = {lat: position.coords.latitude,lng: position.coords.longitude};
-  var to = {lat:-34.92874438537443, lng:138.5987412929535};
   var curr = new H.map.Marker(currentPos);
   map.addObject(curr);
 
+  map.setZoom(zoomLevel-2);
   map.setCenter(currentPos);
-  map.setZoom(zoomLevel);
-  calculateRouteFromAtoB(currentPos, to);
+  count = 0;
+  getData(function(response){
+    for(var i in response) {
+      count++;
+     
+      var to = {lat:response[i].lat, lng:response[i].lng};
+      calculateRouteFromAtoB(currentPos, to, addRoute2Array);  
+    }
+    //wait for all routes to finish first
+  });
 }
 function error() {
   alert("Unable to retrieve your location");
 };
 // Routing
-function calculateRouteFromAtoB (from, to) {
+function calculateRouteFromAtoB (from, to, callback) {
+
   var router = platform.getRoutingService(null, 8),
   routeRequestParams = {
     routingMode: 'fast',
@@ -68,15 +120,47 @@ function calculateRouteFromAtoB (from, to) {
   };
   router.calculateRoute(
     routeRequestParams,
-    onSuccess,
+    callback,
     onError
   );
 }
-function onSuccess(result) {
-  var route = result.routes[0];
-  addRouteShapeToMap(route);
-  addManueversToMap(route);
-  addSummaryToPanel(route);
+function addRoute2Array(result) {
+ routes.push(result.routes[0]);
+  idx++;
+  if (count == idx) {
+    let bestDistance = -1;
+    let bestRouteIndex = 0;
+    let duration = 0, distance = 0;
+    for (var j in routes) {
+      duration = 0;
+      distance = 0;
+      routes[j].sections.forEach((section) => {
+        distance += section.travelSummary.length;
+        duration += section.travelSummary.duration;
+      });
+      if (bestDistance == -1) {
+        bestDistance = distance;
+        bestRouteIndex = j;
+      } else if (bestDistance > distance) {
+        bestDistance = distance;
+        bestRouteIndex = j;
+      }
+    }
+    addRouteShapeToMap(routes[bestRouteIndex]);
+    addManueversToMap(routes[bestRouteIndex]);
+    addSummaryToPanel(routes[bestRouteIndex]);
+   // addWaypointsToPanel(routes[bestRouteIndex]);
+    //addManueversToPanel(routes[bestRouteIndex]);
+  
+    //Within the onResult callback:
+
+// Create an outline for the route polyline:
+
+// create a group that represents the route line and contains
+
+    $('body').addClass('loaded');
+    //calculateRouteFromAtoB(currentPos, to, );
+  }
 }
 function onError(error) {
   alert('Can\'t reach the remote server');
@@ -143,6 +227,20 @@ function addManueversToMap(route){
     map.addObject(group);
   });
 }
+var bubble;
+function openBubble(position, text){
+  if(!bubble){
+     bubble =  new H.ui.InfoBubble(
+       position,
+       // The FO property holds the province name.
+       {content: text});
+     ui.addBubble(bubble);
+   } else {
+     bubble.setPosition(position);
+     bubble.setContent(text);
+     bubble.open();
+   }
+ }
 Number.prototype.toMMSS = function () {
   return  Math.floor(this / 60)  +' minutes '+ (this % 60)  + ' seconds.';
 }
@@ -168,17 +266,7 @@ function addSummaryToPanel(route){
   summaryDiv.style.marginLeft ='5%';
   summaryDiv.style.marginRight ='5%';
   summaryDiv.innerHTML = content;
-  routeInstructionsContainer.appendChild(summaryDiv);
+  panel.appendChild(summaryDiv);
 }
 /*
-var ajax = $.ajax({
-  url : wfsUrl,
-  dataType : 'json',
-  jsonCallback : 'callback',
-  type: 'GET',
-  success : function (response) {
-    console.log(response.features);
-          
-  }
-});
 */
